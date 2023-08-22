50dbe182843712ad25e503216472ac196e5934b8
Nancy Patel <nancypatel5757@gmail.com>
2023-08-22 23:50:14 +0530

    added implementation for diff.cpp and edited multiply.cpp

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class diff{
+    int a,b;
+
+    void setValue(int a, int b){
+        this->a = a;
+        this->b = b;
+
+        cout<<"diff = "<<a-b<<endl;
+    }
+
+};
--- multiply.cpp
+++ multiply.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Sum{
+class multiply{
     int a,b;
 
     void setValue(int a, int b){
