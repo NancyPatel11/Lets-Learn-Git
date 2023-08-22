038b59b5f86cdeb30d3a44e4429e2e8879554a03
Nancy Patel <nancypatel5757@gmail.com>
2023-08-22 23:10:43 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Sum{
+    int a,b;
+
+    void setValue(int a, int b){
+        this->a = a;
+        this->b = b;
+
+        cout<<a+b<<endl;
+    }
+
+};
